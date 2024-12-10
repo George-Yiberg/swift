9cfe72c54d348229db7bc56d0e797a4cc911b162
George Yiberg <George.Yiberg@gmail.com>
2024-12-11 00:07:26 +0800

    add the basics of fundamental data types

---
+++ The Swift Programming Language 6/Language Guide/01_The Basics/Sources/main.swift
@@ -0,0 +1,22 @@
+﻿// Console app.
+
+print("Hello, World!")
+
+
+let i:Int = 5
+let d:Double = 9.9
+let b:Bool = true
+let s:String = "This is a string"
+
+print(i)
+print(d)
+print(b)
+print(s)
+
+let arrayCollection: Array = [1, 2, 3, 4, 4, 4]
+let setCollection: Set = [1, 2, 3, 4, 4, 4]
+let dictionaryCollection: [Int:String] = [1:"one", 2:"two", 3:"three"]
+print(arrayCollection)
+// [2, 3, 4, 1]
+print(setCollection)
+print(dictionaryCollection)
